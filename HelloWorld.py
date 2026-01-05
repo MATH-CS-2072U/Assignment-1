c4c2b3fbf238f83da04de30284b2eabd35ca0f7a
azar-shakoori <azar.shakoori@ontariotechu.net>
2026-01-05 09:13:18 -0500

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,8 @@
+# Assignment 1 Template file
+
+# Student Name:
+# Student ID:
+# Collaborators (if any):
+
+def HelloWorld(name):
+  # Your code goes here
---
+++ hello_test.py
@@ -0,0 +1,4 @@
+import HelloWorld;
+
+def test_hello():
+    assert HelloWorld.HelloWorld("Franz Kafka") == "Hello Franz Kafka!"
